2b80adb41b58ef3a998e2af8b902bb5d33bbb2b0
Fabio Santangelo <fabbomaui04gmail.com>
2024-11-16 15:28:53 +0100

    Fine esercizio

---
+++ main.js
@@ -0,0 +1,20 @@
+let num1=(1);
+
+let num2=(2);
+
+console.log(num1);
+
+console.log(num2);
+
+let stringa=(`Ciaooo`);
+
+stringa=(`arrivederci`);
+
+console.log(stringa);
+
+const PIGRECO=(3.14159265359);
+
+PIGRECO=(4);
+
+console.log(PIGRECO);
+
